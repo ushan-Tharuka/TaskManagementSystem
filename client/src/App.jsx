686685ee36d2a93637cc1e7bd9a0e7516d58bcb3
ushan-Tharuka <ushantharuka2222@gmail.com>
2024-12-01 19:57:15 +0530

    add routers to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import Login from "./pages/Login";
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
 
 function App() {
-  return <Login/>;
+  return (
+    <main className='w-full min-h-screen bg-[#f3f4f6]'>
+
+      <Routes>
+      <Route element={<Layout />}>
+          <Route index path='/' element={<Navigate to='/dashboard' />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='/completed/:status' element={<Tasks />} />
+          <Route path='/in-progress/:status' element={<Tasks />} />
+          <Route path='/todo/:status' element={<Tasks />} />
+          <Route path='/team' element={<Users />} />
+          <Route path='/trashed' element={<Trash />} />
+          <Route path='/task/:id' element={<TaskDetails />} />
+        </Route>
+          <Route path='/log-in' element={<Login />} />  
+      </Routes>
+    </main>
+  );
 }
 
 export default App
